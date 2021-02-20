7ad03584746e53ed3d6deab39a8f15fcd7f977c7
Abhinav Gupta <79180704+AbhinavGupta2002@users.noreply.github.com>
2021-02-20 23:36:42 +0400

    Update Code.c

--- Code.c
+++ Code.c
@@ -265,7 +265,8 @@ int main (void) {
   printf("%c[1;33mand Player 2's moves are represented as ", 0x1b);
   printf("%c[0;32mO\e[m", 0x1b);
   printf("%c[1;33m.\n\e[m", 0x1b);  
-  printf("%c[1;33m              The values of the number of rows, number of columns and length must be greater than 2.\n\n\e[m", 0x1b);
+  printf("%c[1;33m              The values of the number of rows, number of columns and length must be greater than 2.\n\e[m", 0x1b);
+    printf("%c[1;33m              The value of length must be less than or equal to max(number of columns, number of rows).\n\n\e[m", 0x1b);
   printf("Enter Number Of Columns: ");
   scanf("%d", &width);
   printf("Enter Number Of Rows: ");
