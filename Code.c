7d53385d887146b203bf58b279ba1cf8ed5c19d9
Abhinav Gupta <79180704+AbhinavGupta2002@users.noreply.github.com>
2021-02-21 10:51:58 +0400

    Update Code.c

--- Code.c
+++ Code.c
@@ -294,7 +294,7 @@ int main (void)
       printf ("Enter Length: ");
       scanf ("%d", &length);
       while (length <= 1 || length > ((width >= height) ? width : height)) {
-	printf("Please enter valid length: ");
+	printf("Please enter a valid length: ");
 	scanf("%d", &length);
       }
       flag = connect_analysis (width, height, length);
@@ -318,7 +318,7 @@ int main (void)
 	  printf ("%c[0;31mCongratulations Player 1!\n\e[m", 0x1b);
 	  printf ("%c[0;31mYou won on move %d.\n\e[m", 0x1b, abs (flag));
 	}
-	printf("If you want to continue enter 0 otherwise enter 1: ");
+	printf("If you wish to play again, enter 0. Otherwise, enter 1: ");
 	scanf("%d", &cont);
 	if(cont != 0) {
 	    exit(0);
